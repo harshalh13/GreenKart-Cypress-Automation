831f3f9b95154f8f1c13bb62a3a3cc32b6649048
harshalh13 <harshalhajare13@gmail.com>
2024-05-08 22:31:21 +0530

    cc

--- firstcode.js
+++ firstcode.js
@@ -1,7 +1,7 @@
 let gamenum= 2;
-let usernum = prompt("guessss the correct number??")
+let usernum = prompt("guess the correct number??")
 do{
- usernum = prompt("you have guessed the wrong number, enter a number again");
+ usernum = prompt("you have guesssed the wrong number, enter a number again");
 }while(usernum != gamenum);
 
-console.log("you have guessed the number correctly")
+console.log("you have guessed the number correctly!!")
